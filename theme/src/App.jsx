42aaca3bae2ddc31e86915515ded2704c0c32cfd
pwamoreno <dontreader23@gmail.com>
2023-12-31 08:34:36 +0100

    First commit

---
+++ theme/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import DarkMode from './components/Darkmode/DarkMode';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <DarkMode />
+      </div>
+    </>
+  )
+}
+
+export default App
